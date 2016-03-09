1ce03fac88cb4901825ac8b326e3a33897750fb2
estebanmjm <mateojm77@gmail.com>
2016-03-09 17:55:11 -0500

    ya guarda schedules

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -69,6 +69,6 @@ class RoutesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def route_params
-      params.require(:route).permit(:origin, :destiny, :code, :cost, { city_ids: [] })
+      params.require(:route).permit(:origin, :destiny, :code, :cost, { city_ids: [] }) 
     end
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -69,6 +69,6 @@ class SchedulesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def schedule_params
-      params.require(:schedule).permit(:date, :route_ids, :city_ids)
+      params.require(:schedule).permit( :date, :route_id, :airship_id)
     end
 end
